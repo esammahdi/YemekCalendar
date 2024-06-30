6f8f76eaf4e7341edc2f2c0050f5e4d6de6f20ed
esammahdi <esammahdi111@gmail.com>
2024-06-30 18:27:32 +0300

    Enclosed the redirection text inside a Row to make it centered on wide screens.

--- app/src/main/java/com/example/yemekcalendar/authentication/presentation/components/Redirection.kt
+++ app/src/main/java/com/example/yemekcalendar/authentication/presentation/components/Redirection.kt
@@ -1,9 +1,12 @@
 package com.example.yemekcalendar.authentication.presentation.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import com.example.yemekcalendar.ui.theme.RegularFont
@@ -14,12 +17,18 @@ fun Redirection(
     text: String,
     onClick: () -> Unit
 ) {
-    Text(
-        modifier = modifier
-            .clickable { onClick() },
-        text = text,
-        fontWeight = FontWeight.Bold,
-        color = MaterialTheme.colorScheme.secondary,
-        fontFamily = RegularFont
-    )
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            modifier = modifier
+                .clickable { onClick() },
+            text = text,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.secondary,
+            fontFamily = RegularFont
+        )
+    }
 }
