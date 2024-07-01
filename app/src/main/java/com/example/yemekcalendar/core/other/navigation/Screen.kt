c5a0b6e475cbeef930b1c5f039a3405c1c52655a
esammahdi <esammahdi111@gmail.com>
2024-07-01 07:12:35 +0300

    Removed redundant old commented code.

--- app/src/main/java/com/example/yemekcalendar/core/other/navigation/Screen.kt
+++ app/src/main/java/com/example/yemekcalendar/core/other/navigation/Screen.kt
@@ -1,20 +1,6 @@
 package com.example.yemekcalendar.core.other.navigation
 
 import kotlinx.serialization.Serializable
-
-//sealed class Screens(val route: String) {
-//    object LoginScreen : Screens(route = "Login_Screen")
-//    object RegisterationScreen : Screens(route = "Registration_Screen")
-//    object ResetPasswordScreen : Screens(route = "ResetPassword_Screen")
-//    object HomeScreen : Screens(route = "Home_Screen")
-//    object CalendarScreen : Screens(route = "Calendar_Screen")
-//    object SettingsScreen : Screens(route = "Settings_Screen")
-//    object FoodItemScreen : Screens(route = "FoodItem_Screen")
-//    object FoodItemsScreen : Screens(route = "FoodItems_Screen/{foodItemId}")
-//    object ProfileScreen : Screens(route = "Profile_Screen")
-//    object AboutScreen : Screens(route = "About_Screen")
-//}
-
 sealed class Screen() {
     @Serializable
     object LoginScreen : Screen()
