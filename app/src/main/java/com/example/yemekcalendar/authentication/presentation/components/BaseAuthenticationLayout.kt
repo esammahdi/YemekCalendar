8a550ea733a830c4ab563d5f2a5f5634db4f0444
esammahdi <esammahdi111@gmail.com>
2024-07-02 19:30:55 +0300

    Created a base layout for the auth screens that wraps a `system bar padding` aware language dropdown list, and a scrollable column layout that pays attention to `Ime Padding`.

---
+++ app/src/main/java/com/example/yemekcalendar/authentication/presentation/components/BaseAuthenticationLayout.kt
@@ -0,0 +1,69 @@
+package com.example.yemekcalendar.authentication.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.yemekcalendar.settings.presentation.viewmodel.Languages
+
+@Composable
+fun BaseAuthenticationLayout(
+    selectedLanguage: Languages,
+    onLanguageSelected: (Languages) -> Unit,
+    content: @Composable ColumnScope.() -> Unit
+) {
+
+    val scrollState = rememberScrollState()
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .statusBarsPadding(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+        )
+        {
+            LanguageDropdownList(
+                selectedLanguage = selectedLanguage,
+                onLanguageSelected = { onLanguageSelected(it) }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .imePadding()
+                .verticalScroll(scrollState),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 30.dp, end = 30.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                content()
+            }
+        }
+    }
+}
