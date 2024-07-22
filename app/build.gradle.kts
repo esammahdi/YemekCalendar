57c1b4346a6eb700b2fc32054ae806632cc215f9
esammahdi <esammahdi111@gmail.com>
2024-07-22 18:23:20 +0300

    Changed `isMinifyEnabled` and `isShrinkResources` values to false because code shrinking was removing required classes and thus causing the app to crash.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
